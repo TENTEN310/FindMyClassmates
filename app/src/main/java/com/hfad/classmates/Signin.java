1432257a347fd85c85c56fa1bd22e0fe3c8bc790
Changzhong Qian <sidqian@usc.edu>
2023-10-18 18:53:13 -0700

    added register and signin

---
+++ app/src/main/java/com/hfad/classmates/Signin.java
@@ -0,0 +1,14 @@
+package com.hfad.classmates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
