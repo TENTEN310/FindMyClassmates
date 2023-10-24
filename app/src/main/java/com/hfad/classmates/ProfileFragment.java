998c7d90391a95ee73d811177c983db04a70939d
Eugenia <eugeniadru@gmail.com>
2023-10-24 14:17:54 -0700

    email dynamically uploads to profile

--- app/src/main/java/com/hfad/classmates/ProfileFragment.java
+++ app/src/main/java/com/hfad/classmates/ProfileFragment.java
@@ -7,58 +7,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        //initialize the rootView
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //initialize the variables
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        TextView nameText = rootView.findViewById(R.id.nameText);
+        TextView emailText = rootView.findViewById(R.id.emailText);
+        ImageView profilePicture = rootView.findViewById(R.id.profilePicture);
+
+        //set the profile picture, name, and email
+        nameText.setText(user.getDisplayName()); //need the name
+        emailText.setText(user.getEmail());
+
+        Glide
+                .with(this)
+                .load(user.getPhotoUrl()).apply(RequestOptions.circleCropTransform()) //need the photo
+                .into(profilePicture);
+
+        //return our view
+        return rootView;
     }
 }
