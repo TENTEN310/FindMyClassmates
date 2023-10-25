78e7a92ec163342150a6eb1f8d43e6adda06fd87
Changzhong Qian <sidqian@usc.edu>
2023-10-25 15:09:55 -0700

    fixed refresh problem

--- app/src/main/java/com/hfad/classmates/MainActivity.java
+++ app/src/main/java/com/hfad/classmates/MainActivity.java
@@ -31,33 +31,37 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView navigationView;
     ActionBar toolbar;
     ImageView profileAvatar;
+    HomeFragment homeFragment = new HomeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    ClassesFragment classesFragment = new ClassesFragment();
+    Chats chatsFragment = new Chats();
+    PostFragment postFragment = new PostFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar my_toolbar = (Toolbar)findViewById(R.id.myToolbar);
+        setContentView(R.layout.activity_main); // This needs to be at the top
 
-
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.myToolbar);
         setSupportActionBar(my_toolbar);
         toolbar = getSupportActionBar();
-        navigationView = findViewById(R.id.navigation);
+
+        navigationView = findViewById(R.id.navigation); // Initialize after setContentView
         navigationView.setOnItemSelectedListener(selectedListener);
         toolbar.setTitle("FindMyClassmates");
 
-        // Default to the home page upon opening the app
-        HomeFragment fragment = new HomeFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.content, fragment, "");
-        fragmentTransaction.commit();
+        initializeFragments();
 
+        // Default to the home page upon opening the app
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.show(homeFragment);
+        fragmentTransaction.commit();
     }
 
 
-    public boolean onCreate(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.top, menu);
         MenuItem menuItem = menu.findItem(R.id.profile_image);
-        View view = MenuItemCompat.getActionProvider(menuItem).onCreateActionView();
         profileAvatar = (ImageView) findViewById(R.id.tool_bar_profile_image);
         FirebaseUtil.getProfilePic(getUserID()).getDownloadUrl()
                 .addOnCompleteListener(task -> {
@@ -69,61 +73,78 @@ public class MainActivity extends AppCompatActivity {
         profileAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "profile clicked", Toast.LENGTH_SHORT);
+                //Toast.makeText(MainActivity.this, "profile clicked", Toast.LENGTH_SHORT).show();
             }
         });
         return super.onCreateOptionsMenu(menu);
+    }
+
+
+    private void initializeFragments() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.add(R.id.content, homeFragment, "HOME");
+        fragmentTransaction.add(R.id.content, profileFragment, "PROFILE");
+        fragmentTransaction.add(R.id.content, classesFragment, "CLASSES");
+        fragmentTransaction.add(R.id.content, chatsFragment, "CHATS");
+        fragmentTransaction.add(R.id.content, postFragment, "POST");
+
+        fragmentTransaction.hide(profileFragment);
+        fragmentTransaction.hide(classesFragment);
+        fragmentTransaction.hide(chatsFragment);
+        fragmentTransaction.hide(postFragment);
+
+        fragmentTransaction.commit();
     }
 
     private BottomNavigationView.OnItemSelectedListener selectedListener = new BottomNavigationView.OnItemSelectedListener() {
         @SuppressLint("NonConstantResourceId")
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+            // Hide all fragments
+            fragmentTransaction.hide(homeFragment);
+            fragmentTransaction.hide(profileFragment);
+            fragmentTransaction.hide(classesFragment);
+            fragmentTransaction.hide(chatsFragment);
+            fragmentTransaction.hide(postFragment);
+
             switch (menuItem.getItemId()) {
                 case R.id.nav_home:
                     toolbar.setTitle("FindMyClassmates");
-                    HomeFragment fragment = new HomeFragment();
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.content, fragment, "");
-                    fragmentTransaction.commit();
-                    return true;
+                    fragmentTransaction.show(homeFragment);
+                    break;
 
                 case R.id.nav_profile:
                     toolbar.setTitle("FindMyClassmates");
-                    ProfileFragment fragment1 = new ProfileFragment();
-                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction1.replace(R.id.content, fragment1);
-                    fragmentTransaction1.commit();
-                    return true;
+                    fragmentTransaction.show(profileFragment);
+                    break;
 
                 case R.id.nav_classes:
                     toolbar.setTitle("FindMyClassmates");
-                    ClassesFragment fragment2 = new ClassesFragment();
-                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction2.replace(R.id.content, fragment2, "");
-                    fragmentTransaction2.commit();
-                    return true;
+                    fragmentTransaction.show(classesFragment);
+                    break;
 
                 case R.id.nav_chats:
                     toolbar.setTitle("FindMyClassmates");
-
-                    Chats listFragment = new Chats();
-                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction3.replace(R.id.content, listFragment, "");
-                    fragmentTransaction3.commit();
-                    return true;
+                    fragmentTransaction.show(chatsFragment);
+                    break;
 
                 case R.id.nav_post:
                     toolbar.setTitle("FindMyClassmates");
-                    PostFragment fragment4 = new PostFragment();
-                    FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction4.replace(R.id.content, fragment4, "");
-                    fragmentTransaction4.commit();
-                    return true;
+                    fragmentTransaction.show(postFragment);
+                    break;
+
+                default:
+                    return false;
             }
-            return false;
+
+            fragmentTransaction.commit();
+            return true;
         }
     };
+
 }
 
 
