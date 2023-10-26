4b7d85e1b0943419a463e45fb726e2623f1f4b39
Eugenia <eugeniadru@gmail.com>
2023-10-26 16:12:27 -0700

    fixed classes bug

--- app/src/main/java/com/hfad/classmates/ClassesFragment.java
+++ app/src/main/java/com/hfad/classmates/ClassesFragment.java
@@ -18,40 +18,6 @@ import java.util.List;
 
 public class ClassesFragment extends Fragment {
 
-    public class Class {
-        private String name;
-        private String professor;
-        private String subject;
-        private String description;
-
-        public Class() {
-            // Default constructor required for Firestore
-        }
-
-        public Class(String name, String professor, String subject, String description) {
-            this.name = name;
-            this.professor = professor;
-            this.subject = subject;
-            this.description = description;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getProfessor() {
-            return professor;
-        }
-
-        public String getSubject() {
-            return subject;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-    }
-
     public ClassesFragment() {
         // Required empty public constructor
     }
