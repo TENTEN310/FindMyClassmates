4e93227449708518de91840a9806f65d38b1692f
Eugenia <eugeniadru@gmail.com>
2023-10-29 12:52:47 -0700

    implemented uploading the materials to storage

--- app/src/main/java/com/hfad/classmates/ClassDetail.java
+++ app/src/main/java/com/hfad/classmates/ClassDetail.java
@@ -1,44 +1,35 @@
 package com.hfad.classmates;
 
-
-import static androidx.fragment.app.FragmentManager.TAG;
-
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Transaction;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.hfad.classmates.objectClasses.Classes;
 import com.hfad.classmates.objectClasses.ProfileInfo;
 import com.hfad.classmates.util.FirebaseUtil;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
 public class ClassDetail extends AppCompatActivity {
     TextView classFullName, professor, classAbv, description, empty1, empty2;
     Classes classes;
-
     ImageButton back, add, remove;
-
-    Button roster;
+    Button roster, uploadMaterial;
+    private Uri fileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +41,7 @@ public class ClassDetail extends AppCompatActivity {
         classAbv = findViewById(R.id.class_name);
         back = findViewById(R.id.back_btn);
         roster = findViewById(R.id.button);
+        uploadMaterial = findViewById(R.id.uploadMaterialbtn);
         add = findViewById(R.id.imageButton2);
         remove = findViewById(R.id.imageButton4);
         remove.setVisibility(Button.GONE);
@@ -74,6 +66,65 @@ public class ClassDetail extends AppCompatActivity {
         checkIfUserIsInClass(classes.getAbv(), FirebaseUtil.getUserID(), add, remove);
         remove.setOnClickListener(v -> removeClassFromUserAndUserFromClass(classes, add, remove));
 
+        // add a file to the class materials
+        uploadMaterial.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*"); // all files are allowed
+            startActivityForResult(intent, 1);
+        });
+    }
+
+    // add the file to the database storage
+    private void uploadFileWithCustomName(Uri fileUri, String customFileName) {
+        if (fileUri != null) {
+            String fileName = classes.getAbv() + "/" + customFileName;
+
+            // create the location for this file
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference()
+                    .child("materials")
+                    .child(classes.getAbv())
+                    .child(customFileName);
+
+            // store the file here
+            storageRef.putFile(fileUri)
+                    // upload success
+                    .addOnSuccessListener(taskSnapshot -> {
+                        Toast.makeText(getApplicationContext(), "File uploaded successfully", Toast.LENGTH_SHORT).show();
+                    })
+                    // upload failure
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(getApplicationContext(), "Upload failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "Please select a file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            fileUri = data.getData();
+
+            // set the name for the file
+            final EditText customFileNameEditText = new EditText(this);
+            customFileNameEditText.setHint("Enter file name");
+
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Custom File Name");
+            alertDialog.setView(customFileNameEditText);
+            alertDialog.setPositiveButton("Upload", (dialog, which) -> {
+                String customFileName = customFileNameEditText.getText().toString().trim();
+                if (!customFileName.isEmpty() && fileUri != null) {
+                    uploadFileWithCustomName(fileUri, customFileName);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter a custom file name", Toast.LENGTH_SHORT).show();
+                }
+            });
+            alertDialog.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+            alertDialog.show();
+        }
     }
 
     private void checkIfUserIsInClass(String classID, String userID, ImageButton add, ImageButton remove) {
@@ -100,7 +151,6 @@ public class ClassDetail extends AppCompatActivity {
             }
         });
     }
-
 
     private void addClassToUserAndUserToClass(Classes currentClass, ImageButton add, ImageButton remove) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -145,7 +195,6 @@ public class ClassDetail extends AppCompatActivity {
                                     if (userProfile != null) {
                                         transaction.set(classUserRef, userProfile);
                                     }
-
                                     return null;
                                 })
                                 .addOnSuccessListener(aVoid -> {
@@ -156,7 +205,6 @@ public class ClassDetail extends AppCompatActivity {
                                 .addOnFailureListener(e -> {
                                     Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                                 });
-
                     } else {
                         Toast.makeText(getApplicationContext(), "User profile not found!", Toast.LENGTH_SHORT).show();
                     }
@@ -165,7 +213,6 @@ public class ClassDetail extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Error fetching user profile: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                 });
     }
-
 
     private void removeClassFromUserAndUserFromClass(Classes currentClass, ImageButton add, ImageButton remove) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -214,8 +261,4 @@ public class ClassDetail extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                 });
     }
-
-
-
 }
-
